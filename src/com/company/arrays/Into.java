404cb92e3b29d5fc7f4d2f6287a0d6559d394e4a
manvi0308 <manvi.tca1857033@tmu.ac.in>
2021-08-13 18:20:28 +0530

    Added second program

--- src/com/company/arrays/Into.java
+++ src/com/company/arrays/Into.java
@@ -1,3 +1,14 @@
-package com.company.arrays;public class Into
+package com.company.arrays;
+import java.util.Scanner;
+
+public class Into
 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Testing files");
+        int a = sc.nextInt();
+        System.out.print(a);
+        sc.close();
+
+    }
 }
