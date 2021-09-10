8247da7597c6d9a28ce7790becf5e0ef8fdc12bf
Manvi Chaddha <60390722+manvi0308@users.noreply.github.com>
2021-09-10 07:01:00 +0000

    Added Contain Duplicates

---
+++ ContainDuplicates/ContainDuplicates.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+public class ContainDuplicates
+{
+    public static boolean isDuplicate(int[] arr)
+    {
+        Arrays.sort(arr);
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] == arr[i + 1])
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+public static void main(String[] args) {
+    int[] arr = {1, 2, 4, 5, 5, 1, 6, 7, 8};
+    System.out.println(isDuplicate(arr));
+}
+}
