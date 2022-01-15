420518a20913849f582f894fd8d0756413e6d70d
Manvi Chaddha <60390722+manvi0308@users.noreply.github.com>
2022-01-15 18:25:16 +0530

    Add files via upload

--- findduplicates/FindDuplicates.java
+++ findduplicates/FindDuplicates.java
@@ -1,1 +1,36 @@
+/**
+ * 
+ */
+package findduplicates;
+import java.util.HashSet;
 
+/**
+ * @author manvi0308
+ *
+ */
+public class FindDuplicates {
+
+	/**
+	 * @param args
+	 */
+	// https://leetcode.com/problems/find-the-duplicate-number
+	 public static int findDuplicate(int[] nums) {
+	        HashSet<Integer> set = new HashSet<>();
+	        int dup = -1;
+	        for(int i = 0; i < nums.length;i++) {
+	        	if(!set.contains(nums[i])) {
+	        		set.add(nums[i]);
+	        	}
+	        	else {
+	        		dup = nums[i];
+	        	}
+	        }
+	        return dup;
+	    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {1, 1, 2, 3, 4, 5};
+		System.out.println(findDuplicate(arr));
+	}
+
+}
