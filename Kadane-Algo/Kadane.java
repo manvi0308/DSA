d803a27a46d0c82dbfb7b94d830600b2cd5150f8
Manvi Chaddha <60390722+manvi0308@users.noreply.github.com>
2022-01-15 18:34:14 +0530

    Add files via upload

--- Kadane-Algo/Kadane.java
+++ Kadane-Algo/Kadane.java
@@ -1,1 +1,30 @@
+package kadane;
 
+public class Kadane {
+	long maxSubarraySum(int arr[], int n) {
+		// Your code here
+		long max_so_far = Integer.MIN_VALUE;
+		long max_ending_here = 0;
+
+		for (int i = 0; i < n; i++) {
+			max_ending_here = max_ending_here + arr[i];
+
+			if (max_ending_here < 0) {
+				max_ending_here = 0;
+			}
+
+			if (max_so_far < max_ending_here) {
+				max_so_far = max_ending_here;
+			}
+		}
+
+		return max_so_far;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
